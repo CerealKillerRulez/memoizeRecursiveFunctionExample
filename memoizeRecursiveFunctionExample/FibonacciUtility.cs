aea1886013ab48d51d7dcf13fd39ced6169d7195
Michele Panipucci <mail@michelepanipucci.it>
2021-07-01 00:03:25 +0200

    Add project files.

---
+++ memoizeRecursiveFunctionExample/FibonacciUtility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace memoizeRecursiveFunctionExample
+{
+    public class FibonacciUtility
+    {
+        public static Dictionary<ulong, ulong> cache = new Dictionary<ulong, ulong>();
+        private static ulong ans;
+
+        public FibonacciUtility() {
+
+        }
+
+        public ulong FibonacciSum(ulong n)
+        {
+            if (n <= 0) 
+                return 1;
+
+            return n > 2 ? FibonacciSum(n - 1) + FibonacciSum(n - 2) : 1;
+        }
+
+        public ulong FibonacciSumMemoized(ulong n)
+        {
+            if (cache.ContainsKey(n))
+                return cache[n];
+
+            if (n <= 0)
+                return 1;
+
+            ans = n > 2 ? FibonacciSumMemoized(n - 1) + FibonacciSumMemoized(n - 2) : 1;
+            cache[n] = ans;
+
+            return ans;
+        }
+
+    }
+}
---
+++ memoizeRecursiveFunctionExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace memoizeRecursiveFunctionExample
+{
+    class Program
+    {
+        private static readonly FibonacciUtility utility = new FibonacciUtility();
+
+        static void Main(string[] args)
+        {
+            ulong max = 5000;
+
+            ReportExecution(() => {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine($"Fibonacci memoizzata = {utility.FibonacciSumMemoized(max):N0}");
+            });
+
+            ReportExecution(() => {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Fibonacci = {utility.FibonacciSum(max):N0}");
+            });
+
+            Console.ReadKey();
+        }
+
+        private static void ReportExecution(Action action)
+        {
+            var startDate = DateTime.Now;
+
+            action();
+
+            Console.WriteLine($"Elaborato in {DateTime.Now.Subtract(startDate).TotalSeconds:N2} secondi.");
+            Console.WriteLine();
+        }
+    }
+}
